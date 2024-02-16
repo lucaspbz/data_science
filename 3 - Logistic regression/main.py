d43f7865cb6e3405fbe47934e4560ff6eb5c4778
Lucas Bezerra <lpbezerra@ehnusa.com>
2024-02-16 12:14:55 -0300

    Decision Tree

--- 3 - Logistic regression/main.py
+++ 3 - Logistic regression/main.py
@@ -1,7 +1,6 @@
 import numpy as np
 from sklearn.model_selection import train_test_split
 from sklearn import datasets
-import matplotlib.pyplot as plt
 
 
 def sigmoid(x):
---
+++ 4 - Decision Tree/README.md
@@ -0,0 +1,5 @@
+Decision Tree
+
+https://www.youtube.com/watch?v=NxEHSAfFlK8&list=PLcWfeUsAys2k_xub3mHks85sBHZvg24Jd&index=5
+
+Used when the data has many features
